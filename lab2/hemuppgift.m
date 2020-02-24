8b66a693f1ddc45ec711f5b69f7f292e6a9b2eb9
blennster <emil.blennow99@gmail.com>
2020-02-24 10:38:18 +0000

    small fix

--- lab2/hemuppgift.m
+++ lab2/hemuppgift.m
@@ -1,5 +1,5 @@
 % m = complexmat(1000, -2 + 1i, 1 - 1i);
-xm = complexmat(1000, -0.7 + 0.7i, -0.5 + 0.6i);
+m = complexmat(1000, -0.7 + 0.7i, -0.5 + 0.6i);
 % m = complexmat(1000, -1.8 + 0.03i, -1.5 - 0.03i);
 v = arrayfun(@converge, m);
 
