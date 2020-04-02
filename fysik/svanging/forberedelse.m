8736f7ffad565c179bc6540a18ad1b0b9acff4c9
blennster <emil.blennow99@gmail.com>
2020-04-02 17:50:23 +0200

    Uppgift 2 klar på labb 2

--- fysik/svanging/forberedelse.m
+++ fysik/svanging/forberedelse.m
@@ -1,24 +1,28 @@
-%% Konstanter
 clear
+clc
 
+%% Konstanter
 m = 0.1;
 k = 40;
 dx = 14; % i mmm
 omega_sqr = k / m;
+omega = sqrt(omega_sqr);
+b = 0.4;
+
 x = linspace(0, 1, 1000);
 
 
 %% Uppgift 1
-xt = @(t) dx * cos(sqrt(omega_sqr)*t);
+
+figure(1) % 1.a
+xt = @(t) dx * cos(omega*t);
 
 plot(x, xt(x))
 title('Position mot tid')
 xlabel('\itt\rm / s')
 ylabel('\itx\rm / mm')
 
-
-%% Uppgift 2
-
+figure(2) % 1.b
 vt = diff(xt(x));
 vt = [0 vt]; % Eftersom diff bara är numerisk derivata så finns inte 0 med
 
@@ -27,10 +31,31 @@ title('Fart mot tid')
 xlabel('\itt\rm / s')
 ylabel('\itv\rm / mm/s')
 
-
-%% Uppgift 3
-
+figure(3) % 1.c
 plot(xt(x), vt)
 title('Fart mot position')
 xlabel('\itx\rm / mm')
 ylabel('\itv\rm / mm/s')
+
+%% Uppgift 2
+figure(4)
+
+t_halfA = log(1/2) * 2 * m / -b;
+
+omega_prim = omega * sqrt(1 - (b / (2 * m * omega))^2);
+
+xt = @(t) dx * exp(-t * (b/(2 * m))) .* cos(omega_prim * t);
+
+plot(x, xt(x))
+xline(t_halfA)
+title('Position mot tid')
+xlabel('\itt\rm / s')
+ylabel('\itx\rm / mm')
+
+figure(5)
+vt = diff(xt(x));
+vt = [0 vt];
+
+plot(xt(x), vt)
+
+%% Uppgift 3
